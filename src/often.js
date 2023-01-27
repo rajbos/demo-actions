1eccb982f55364d5cf7ab5ad430109aa5ddf7287
Rob Bos <rajbos@users.noreply.github.com>
2023-01-27 15:48:44 +0100

    Update often.js

--- src/often.js
+++ src/often.js
@@ -9,23 +9,23 @@ module.exports = async ({github, context, owner, repo, workflow}) => {
     try {
         console.log(`Start of the try call`)
 
-        // const runs = await github.rest.actions.getWorkflow({
-        //     owner,
-        //     repo,
-        //     workflow_id
-        // });
-        // console.log(``)
-        // console.log(`Runs: ${JSON.stringify(runs)}`)
-        // console.log(``)
-        // console.log(`Found [${runs.length}] workflow runs`)
+        const runs = await github.rest.actions.getWorkflow({
+            owner,
+            repo,
+            workflow_id
+        });
+        console.log(``)
+        console.log(`Runs: ${JSON.stringify(runs)}`)
+        console.log(``)
+        console.log(`Found [${runs.length}] workflow runs`)
         
-        // const runs2 = await github.rest.actions.listWorkflowRunsForRepo({
-        //         owner,
-        //         repo
-        //     },
-        //     (response) => console.log(`Response: ${JSON.stringify(response)}`)
-        // )        
-        // console.log(`Runs2: ${JSON.stringify(runs2)}`)
+        const runs2 = await github.rest.actions.listWorkflowRunsForRepo({
+                owner,
+                repo
+            },
+            (response) => console.log(`Response: ${JSON.stringify(response)}`)
+        )        
+        console.log(`Runs2: ${JSON.stringify(runs2)}`)
 
         try {
             const response = await github.request(
